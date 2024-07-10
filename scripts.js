0701e1b98e6e92f1bcca26b002bbad2d09141265
Nathan <musevenathan@gmail.com>
2024-07-10 18:18:48 +0300

    styled and applied javaScript to hamburger menu

--- scripts.js
+++ scripts.js
@@ -1,12 +1,28 @@
-let nav = document.getElementsByClassName("nav-links");
-let menu = document.getElementsByClassName("hamburger").addEventListener('click', "mnu()");
+const nav = document.getElementsByClassName("nav-links")[0];
+
+let menu = document.getElementsByClassName("hamburger")[0];
+menu.addEventListener('click', mnu);
 function mnu(){
-  if(nav.style.width = 0) {
-    return nav.style.width = "50%"
-  } else {
-    return nav.style.width = "0%"
+  if(nav.style.width == "0%") {
+    nav.style.width = "50%"
+  }else{
+    nav.style.width = "0%"
   }
+} 
+
+// click anywhere to close menu 
+window.onclick = function(event) {
+  if(event.target === nav) {
+    nav.style.display = "none";
 }
+}
+
+// you can activate x menu and bars 
+// let toggleHamburger =document.getElementsByClassName("hamburger")[0];
+
+// function toggle(x) {
+//   x.classList.toggle("change");
+// }
 
 document.querySelectorAll('input[type="text"], input[type="email"], textarea').forEach((inputs) => {
   inputs.addEventListener('input', () => {
